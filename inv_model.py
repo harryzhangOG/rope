efebe68ded687f7a15cb123e49cef4e14a4e7747
Harry Zhang <harryzhang@Harrys-MacBook-Pro.local>
2020-06-04 14:46:51 -0700

    reverted

--- inv_model.py
+++ inv_model.py
@@ -1,4 +1,5 @@
 import torch
+from builtins import super
 import torchvision
 import numpy as np
 import torch.nn as nn
@@ -8,42 +9,31 @@ import torch.nn.functional as F
 """
 Reimplementation of the inverse model in the paper "Combining Self-Supervised Learning and Imitation for Vision-Based
 Rope Manipulation".
-
 Input: Current state represented by an image or a state tensor
        Target state represented by an image or a state tensor
 Output: Predicted action that can bring the input state to the target state (inverse model)
-
 Network Architecture: Two streams.
                       If the input data are images, then each stream: Conv96, Conv256, Conv384, Conv384, Conv256, Conv200. 
                       SHARE WEIGHTS between the two streams.
-
 We will not use images as states (for now).
 Given we have access to the ground truth rope links locations in Blender:
-
 TODO: Better SA formulations"?
-
 States: 90x2. 90 links in a rope, and each link has x and y.
 Actions: 1x3. 1 time step, dx and dy for the HELD link position.
-
 For details, refer to https://arxiv.org/pdf/1703.02018.pdf
 """
 class Inv_Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(180, 96)
+        self.fc1 = nn.Linear(100, 96)
         self.fc2 = nn.Linear(96, 256)
         self.fc3 = nn.Linear(256, 384)
         self.fc4 = nn.Linear(384, 384)
         self.fc5 = nn.Linear(384, 256)
         self.fc6 = nn.Linear(256, 200)
 
-        # Implemented dim reduction via Bayes as classification.
-        # "Velocity" action output
-        self.fc_out_1 = nn.Linear(400, 1)
-        # X action output
-        self.fc_out_2 = nn.Linear(401, 1)
-        # Y action output
-        self.fc_out_3 = nn.Linear(402, 1)
+        # Did not implement dim reduction via Bayes because the dim is low in this vanilla case.
+        self.fc_out_1 = nn.Linear(400, 3)
     def forward(self, x1, x2):
         # Stream 1
         x1 = F.elu(self.fc1(x1))
@@ -60,14 +50,10 @@ class Inv_Model(nn.Module):
         x2 = F.elu(self.fc5(x2))
         latent2 = F.elu(self.fc6(x2))
         # Concatenate the output latent tensors
+        # TODO: Verify the dimensions. Assuming the batch size dimension is 0
         latent = torch.cat((latent1, latent2), 1)
-        out_frame = F.elu(self.fc_out_1(latent))
-        # Concatenate the latent tensors with the first action pred
-        lat_frame = torch.cat((latent, out_frame), 1)
-        out_x = F.relu(self.fc_out_2(lat_frame))
-        lat_frame_x = torch.cat((lat_frame, out_x), 1)
-        out_y = F.relu(self.fc_out_3(lat_frame_x))
-        return out_frame, out_x, out_y
+        out = self.fc_out_1(latent)
+        return out
 
 
 
--- train_inv_model.py
+++ train_inv_model.py
@@ -9,9 +9,7 @@ import torch.nn.functional as F
 import os
 """
 Training details from the paper:
-
 ADAM Optimizer, lr=1e-4
-
 """
 def train():
     if torch.cuda.is_available():
@@ -26,88 +24,49 @@ def train():
     # Inverse model net
     inv_model = Inv_Model()
     inv_model.to(device)
+    ckpt = 'inv_model_ckpt.pth'
+    checkpoint = torch.load(ckpt, map_location=device)
+    inv_model.load_state_dict(checkpoint['model_state_dict'])
     optimizer = optim.Adam(inv_model.parameters(), lr=1e-4)
     loss_function = nn.MSELoss()
     # TODO: Load Training and Testing data
     path = os.path.join(os.getcwd(), 'states_actions')
-
-    np_load_old = np.load
-    # modify the default parameters of np.load
-    np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
-
     X1 = np.load(os.path.join(path, 's.npy'))
+    print(X1.shape)
     X2 = np.load(os.path.join(path, 'sp1.npy'))
-    Y = np.load(os.path.join(path, 'a_enc.npy'))
-    # Post-process the encoded action to make it compatible with pytorch
-    Y_frame = np.vstack(list(Y[:, 0]))
-    Y_x = np.vstack(list(Y[:, 1]))
-    Y_y = np.vstack(list(Y[:, 2]))
+    Y = np.load(os.path.join(path, 'a.npy'))
     # TODO: Finalize how many training points/validation points we want
-    holdout = 900 
+    holdout = 5500 
     x1 = torch.from_numpy(X1).to(device)
     x1 = torch.reshape(x1, (X1.shape[0], -1))
     x2 = torch.from_numpy(X2).to(device)
     x2 = torch.reshape(x2, (X2.shape[0], -1))
-    y_frame = torch.from_numpy(Y_frame).to(device)
-    y_x = torch.from_numpy(Y_x).to(device)
-    y_y = torch.from_numpy(Y_y).to(device)
-
-    print("PyTorch Data loaded")
-
-    train_dataset = Data.TensorDataset(x1[:holdout], x2[:holdout], y_frame[:holdout], y_x[:holdout], y_y[:holdout])
+    y = torch.from_numpy(Y).to(device)
+    train_dataset = Data.TensorDataset(x1[:holdout], x2[:holdout], y[:holdout])
     train_dataloader = Data.DataLoader(train_dataset, batch_size=200, shuffle=True)
-    val_dataset = Data.TensorDataset(x1[holdout:], x2[holdout:], y_frame[holdout:], y_x[holdout:], y_y[holdout:])
+    val_dataset = Data.TensorDataset(x1[holdout:], x2[holdout:], y[holdout:])
     val_dataloader = Data.DataLoader(train_dataset, batch_size=200, shuffle=True)
     
-    EPOCHS = 1000
+    EPOCHS = 2500
     train_loss = []
     val_loss = []
-    train_acc_frame = []
-    train_acc_x = []
-    train_acc_y = []
-    val_acc_frame = []
-    val_acc_x = []
-    val_acc_y = []
-
     for e in range(EPOCHS):
-        correct_hits_frame = 0
-        correct_hits_x = 0
-        correct_hits_y = 0
-        total = 0
         for i, batch in enumerate(train_dataloader, 0):
             train_x1, train_x2, train_y = batch
-            print(train_x2)
             inv_model.zero_grad()
             optimizer.zero_grad()
             # Train on first HOLDOUT points
             inv_model.train()
-            output_frame, output_x, output_y = inv_model(train_x1.float(), train_x2.float())
-            pred_frame, pred_x, pred_y = torch.max(output_frame.data, 1)[1], torch.max(output_x.data, 1)[1], torch.max(output_y.data, 1)[1]
+            outputs = inv_model(train_x1.float(), train_x2.float())
+            loss = loss_function(outputs, train_y.float())
             
-            total += output_frame.size(0)
-
-            # Convert to labels because CE loss expects class labels
-            train_y_frame_label = torch.max(train_y_frame, 1)[1]
-            train_y_x_label = torch.max(train_y_x, 1)[1]
-            train_y_y_label = torch.max(train_y_y, 1)[1]
-
-            loss_1 = loss_function(output_frame, train_y_frame_label)
-            loss_2 = loss_function(output_x, train_y_x_label)
-            loss_3 = loss_function(output_y, train_y_y_label)
-            
-            correct_hits_frame += (pred_frame==train_y_frame_label).sum().item()
-            correct_hits_x += (pred_frame==train_y_x_label).sum().item()
-            correct_hits_y += (pred_frame==train_y_y_label).sum().item()
-
-            # Aggregate the loss
-            loss = loss_1 + loss_2 + loss_3
             loss.backward()
             # Training loss
             tloss = loss.item()
             optimizer.step()
             train_loss.append(tloss)
-            #if (i % 5) == 0:
-            #    print("Epoch: ", e, "Iteration: ", i, " Training Loss: ", tloss)
+            if (i % 10) == 0:
+                print("Epoch: ", e, "Iteration: ", i, " Training Loss: ", tloss)
         for i, batch in enumerate(val_dataloader, 0):
             # Validate on the rest
             inv_model.eval()
@@ -115,26 +74,24 @@ def train():
             val_outputs = inv_model(val_x1.float(), val_x2.float())
             vloss = loss_function(val_outputs, val_y.float()).item()
             val_loss.append(vloss)
-            #if (i % 5) == 0:
-            #    print("Epoch: ", e, "Iteration: ", i, " Validation Loss: ", vloss)
+            if (i % 10) == 0:
+                print("Epoch: ", e, "Iteration: ", i, " Validation Loss: ", vloss)
 
     torch.save({'model_state_dict': inv_model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict()
                }, 'inv_model_ckpt.pth')
-
-    np.load = np_load_old
-
-    return train_loss, val_loss, train_acc_frame, train_acc_x, train_acc_y, val_acc_frame, val_acc_x, val_acc_y
+    return train_loss, val_loss
 
 if __name__ == "__main__":
     train_loss, val_loss = train()
-    EPOCHS = 1000
-    fig, ax = plt.subplots(figsize=(12, 7))
-    ax.set_title("Loss vs. Epochs")
-    ax.set_xlabel('Epochs')
-    ax.set_ylabel('Loss')
-    ax.plot(np.linspace(0, EPOCHS - 1, EPOCHS), train_loss, np.linspace(0, EPOCHS - 1, EPOCHS), val_loss)
-    ax.legend(['Training Loss', 'Validation Loss'])
-    plt.show()
-    fig.savefig('loss.png')
-
+    np.save('trainloss.npy', train_loss)
+    np.save('valloss.npy', val_loss)
+    EPOCHS = 2500
+  #  fig, ax = plt.subplots(figsize=(12, 7))
+  #  ax.set_title("Loss vs. Epochs")
+  #  ax.set_xlabel('Epochs')
+  #  ax.set_ylabel('Loss')
+  #  ax.plot(np.linspace(0, len(train_loss) - 1, len(train_loss)), train_loss, np.linspace(0, len(val_loss) - 1, len(val_loss)), val_loss)
+  #  ax.legend(['Training Loss', 'Validation Loss'])
+# #   plt.show()
+  #  fig.savefig('loss.png')

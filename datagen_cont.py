50d3a70918e8d0b061a61fd4be4619faa9a0b5c6
Harry Zhang <harryzhang@Harrys-MacBook-Pro.local>
2020-07-30 15:00:33 -0700

    adding augmented ss training and models

---
+++ datagen_cont.py
@@ -0,0 +1,136 @@
+import bpy
+import os
+import json
+import time
+import sys
+import bpy, bpy_extras
+from math import *
+from mathutils import *
+import random
+import numpy as np
+import random
+from random import sample
+import bmesh
+sys.path.append(os.getcwd())
+from blender_rope import *
+from rigidbody_rope import *
+import argparse
+
+def take_action(held_link, at, keyf, settlef):
+    bpy.context.scene.frame_set(bpy.context.scene.frame_current + keyf)
+    held_link.location += Vector((at[0], at[1], at[2]))
+    held_link.keyframe_insert(data_path="location")
+    bpy.context.scene.frame_set(bpy.context.scene.frame_current + settlef)
+    held_link.keyframe_insert(data_path="location")
+
+if "__main__" == __name__:
+    with open("rigidbody_params.json", "r") as f:
+        params = json.load(f)
+    if '--' in sys.argv:
+        argv = sys.argv[sys.argv.index('--') + 1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-num', '--num_iterations', dest='num_iterations', type=int)
+
+    args = parser.parse_known_args(argv)[0]
+    # Number of episodes
+    N = args.num_iterations
+
+    # Source state
+    s = []
+    # Target state
+    sp1 = []
+    # Actions
+    a = []
+
+    clear_scene()
+    add_camera_light()
+    rope = make_rope_v3(params)
+    make_table(params)
+    # bpy.context.scene.gravity *= 10
+    frame_end = 250 * 30
+    
+    bpy.context.scene.rigidbody_world.point_cache.frame_end = frame_end
+    bpy.context.scene.frame_end = frame_end
+
+    held_link = rope[-1]
+    held_link.rigid_body.kinematic = True
+    init_loc = Vector(held_link.location)
+
+    for r in rope:
+        r.rigid_body.mass *= 10
+    r.rigid_body.mass *= 5
+    for seq_no in range(N):
+        print('Experiment Number: ', seq_no)
+        # remove all keyframes
+        bpy.context.scene.frame_set(1)
+        for ac in bpy.data.actions:
+            bpy.data.actions.remove(ac)
+
+        held_link.keyframe_insert(data_path="location")
+        held_link.keyframe_insert(data_path="rotation_euler")
+        bpy.context.scene.rigidbody_world.enabled = True
+        bpy.context.scene.rigidbody_world.point_cache.frame_start = 1
+
+        # set up key frames
+        for action_no in range(20):
+            keyf = 5
+            settlef = 1
+            # dz needs some special handling
+            dz = np.random.uniform(-held_link.location[2], 1)
+            # Record the random action
+            at = np.array([np.random.uniform(0.1, 1.25) * random.choice((-1, 1)), np.random.uniform(0.1, 1.25) * random.choice((-1, 1)), dz])
+            print("Action taken: ", at)
+            a.append(at)
+
+            take_action(held_link, at, keyf, settlef)
+
+        # play back animation
+        endf = bpy.context.scene.frame_current
+        for frame_no in range(1, endf+1):
+            bpy.context.scene.frame_set(frame_no)
+            record_state = (frame_no % 50 == 1)
+            if record_state and frame_no == 1:
+                st = []
+                for r in rope:
+                    st_loc = r.matrix_world.to_translation()
+                    st_vel = [0, 0, 0]
+                    st.append(np.concatenate([np.array(st_loc), np.array(st_vel)]))
+                s.append(st)
+                # Debugging : render the images
+                # bpy.context.scene.render.filepath = '/Users/harryzhang/Desktop/st_%d.jpg'%(frame_no)
+                # bpy.context.scene.camera.location = (0, 0, 60)
+                # bpy.ops.render.render(write_still = True)
+            elif record_state and frame_no != 1 and frame_no != endf:
+                st = []
+                stp1 = []
+                for i, r in enumerate(rope, 0):
+                    st_loc = r.matrix_world.to_translation()
+                    # Calculate velocity
+                    st_vel = (np.array(st_loc) - s[-1][i][:3])/5
+                    st.append(np.concatenate([np.array(st_loc), st_vel]))
+                    stp1.append(np.concatenate([np.array(st_loc), st_vel]))
+                s.append(st)
+                sp1.append(stp1)
+                # Debugging : render the images
+                # bpy.context.scene.render.filepath = '/Users/harryzhang/Desktop/st_%d.jpg'%(frame_no)
+                # bpy.context.scene.camera.location = (0, 0, 60)
+                # bpy.ops.render.render(write_still = True)
+            elif frame_no == endf:
+                stp1 = []
+                for i, r in enumerate(rope, 0):
+                    stp1_loc = r.matrix_world.to_translation()
+                    # Calculate velocity
+                    stp1_vel = (np.array(stp1_loc) - sp1[-1][i][:3])/5 
+                    stp1.append(np.concatenate([np.array(stp1_loc), np.array(stp1_vel)]))
+                sp1.append(stp1)
+                # Debugging : render the images
+                # bpy.context.scene.render.filepath = '/Users/harryzhang/Desktop/st_%d.jpg'%(frame_no)
+                # bpy.context.scene.camera.location = (0, 0, 60)
+                # bpy.ops.render.render(write_still = True)
+    # Save the npy files
+    if not os.path.exists("./states_actions_cont"):
+        os.makedirs('./states_actions_cont')
+    save = os.path.join(os.getcwd(), 'states_actions_cont')
+    np.save(os.path.join(save, 'a.npy'), a)
+    np.save(os.path.join(save, 's.npy'), s)
+    np.save(os.path.join(save, 'sp1.npy'), sp1)
---
+++ train_fwd_model_cont.py
@@ -0,0 +1,109 @@
+from fwd_model import *
+import torch
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torch.utils.data as Data
+import torch.nn.functional as F
+import os
+"""
+Training details from the paper:
+ADAM Optimizer, lr=1e-4
+"""
+def train():
+    if torch.cuda.is_available():
+        device = torch.device("cuda:1")
+        print("Using GPU Cuda 0")
+    else:
+        device = torch.device("cpu")
+        print("Using CPU")
+
+    # Reproducable
+    torch.manual_seed(1)
+    # Forward model net
+    fwd_model = Fwd_Model()
+    fwd_model.to(device)
+    #ckpt = 'fwd_model_ckpt_3d.pth'
+    #checkpoint = torch.load(ckpt, map_location=device)
+    #fwd_model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer = optim.SGD(fwd_model.parameters(), lr=0.001, momentum=0.9)
+    #optimizer = optim.Adam(fwd_model.parameters(), lr=1e-2)
+    
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.8)
+
+    loss_function = nn.MSELoss()
+    # TODO: Load Training and Testing data
+    path = os.path.join(os.getcwd(), 'states_actions_cont')
+    X1 = np.load(os.path.join(path, 's.npy'))
+    print(X1.shape)
+    X2 = np.load(os.path.join(path, 'sp1.npy'))
+    Y = np.load(os.path.join(path, 'a.npy'))
+    # TODO: Finalize how many training points/validation points we want
+    holdout = 30000
+    x1 = torch.from_numpy(X1).to(device)
+    x1 = torch.reshape(x1, (X1.shape[0], -1))
+    x2 = torch.from_numpy(X2).to(device)
+    x2 = torch.reshape(x2, (X2.shape[0], -1))
+    y = torch.from_numpy(Y).to(device)
+    train_dataset = Data.TensorDataset(x1[:holdout], x2[:holdout], y[:holdout])
+    train_dataloader = Data.DataLoader(train_dataset, batch_size=200, shuffle=True)
+    val_dataset = Data.TensorDataset(x1[holdout:], x2[holdout:], y[holdout:])
+    val_dataloader = Data.DataLoader(train_dataset, batch_size=200, shuffle=True)
+    
+    EPOCHS = 1000
+
+    val_loss_fwd = []
+    train_loss_fwd = []
+
+    for e in range(EPOCHS):
+        for i, batch in enumerate(train_dataloader, 0):
+            train_x1, train_x2, train_y = batch
+            fwd_model.zero_grad()
+            optimizer.zero_grad()
+            # Train on first HOLDOUT points
+            fwd_model.train()
+
+            outputs = fwd_model(train_x1.float(), train_y.float())
+            loss = loss_function(outputs, train_x2.float())
+            
+            loss.backward()
+
+            # Training loss
+            tloss = loss.item()
+            optimizer.step()
+
+            train_loss_fwd.append(loss.item())
+            if (i % 40) == 0:
+                print("Epoch: ", e, "Iteration: ", i, " Training Loss Fwd: ", loss.item())
+        for i, batch in enumerate(val_dataloader, 0):
+            # Validate on the rest
+            fwd_model.eval()
+
+            val_x1, val_x2, val_y = batch
+            val_outputs = fwd_model(val_x1.float(), val_y.float())
+
+            vloss = loss_function(val_outputs, val_x2.float()).item()
+
+            val_loss_fwd.append(vloss)
+
+            if (i % 40) == 0:
+                print("Epoch: ", e, "Iteration: ", i, " Validation Loss Fwd: ", vloss)
+        # Adjust learning rate
+        lr_scheduler.step()
+        for param_group in optimizer.param_groups:
+            print("Current learning rate: ", param_group['lr'])
+
+    torch.save({'model_state_dict': fwd_model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()
+               }, 'fwd_model_ckpt_free_end_only.pth')
+    return train_loss_fwd, val_loss_fwd
+
+if __name__ == "__main__":
+    train_loss_fwd, val_loss_fwd = train()
+    if not os.path.exists("./results_cont"):
+        os.makedirs('./results_cont')
+    save_dir = os.path.join(os.getcwd(), 'results_cont')
+    np.save(os.path.join(save_dir, 'trainloss_fwd_cont.npy'), train_loss_fwd)
+    np.save(os.path.join(save_dir, 'valloss_fwd_cont.npy'), val_loss_fwd)
+    EPOCHS = 2000

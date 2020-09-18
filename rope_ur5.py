d3cc3e767ea0dd986f1ff90e1e6874ea31c6259e
Harry Zhang <harryzhang@Harrys-MacBook-Pro.local>
2020-09-17 19:58:17 -0700

    added eval using training

--- rope_ur5.py
+++ rope_ur5.py
@@ -21,6 +21,7 @@ import torch.nn as nn
 from torchvision import models, transforms
 from PIL import Image
 sys.path.append('/Users/harryzhang/Library/Python/3.7/lib/python/site-packages')
+from train_ur5_sim_resnet import DistModel
 from cvxopt import spmatrix, matrix, solvers, printing
 # Sparse matrices. Collects rows, columns, and values as triples
 # to be later passed to the spmatrix function.
@@ -342,6 +343,7 @@ if "__main__" == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument('-num', '--num_iterations', dest='num_iterations', type=int)
     parser.add_argument('-image', '--image', dest='image', type=int)
+    parser.add_argument('-mode', '--mode', dest='mode', type=str)
     args = parser.parse_known_args(argv)[0]
 
     clear_scene()
@@ -380,11 +382,159 @@ if "__main__" == __name__:
 
     N = args.num_iterations
     image = args.image
+    mode = args.mode
 
-    mid_pred = []
-    for seq_no in range(N):
-        print('Experiment Number: ', seq_no)
-        # remove all keyframes
+    if mode == "DATAGEN":    
+
+        for seq_no in range(N):
+            print('Experiment Number: ', seq_no)
+            # remove all keyframes
+
+            bpy.context.scene.frame_set(1)
+            for ac in bpy.data.actions:
+                bpy.data.actions.remove(ac)
+
+            keyf = np.random.randint(10, 20)
+
+            obstacle_height = np.random.uniform(0.5, 4)
+            obstacle_radius = np.random.uniform(0.2, 2)
+            print("Obstacle height %03f, Obstacle radius %03f" %(obstacle_height, obstacle_radius))
+            obstacle_loc = (np.random.uniform(8, 17), -2-np.random.uniform(-0.5, 3), -1+obstacle_height/2)
+            print("Obstacle loc: ", obstacle_loc)
+            bpy.ops.mesh.primitive_cylinder_add(radius=obstacle_radius, rotation=(0, 0, 0), location=obstacle_loc)
+            bpy.ops.rigidbody.object_add()
+            bpy.ops.transform.resize(value=(1,1,obstacle_height/2))
+            cylinder = bpy.context.object
+            cylinder.rigid_body.type = 'PASSIVE'
+            cylinder.rigid_body.friction = 0.7
+
+            if image:
+                mat = bpy.data.materials.new(name="red")
+                mat.diffuse_color = (1, 0, 0, 0)    
+                cylinder.data.materials.append(mat)
+
+            bpy.ops.mesh.primitive_cube_add(location=(23, 0, 0))
+            bpy.ops.rigidbody.object_add(type="PASSIVE")
+            bpy.ops.transform.resize(value=(0.5,10,5))
+
+
+            d2r = pi/180.
+            start_config = np.array([pi/4., 0., pi/6., -pi/4, pi/4., 0.])
+            end_config   = np.array([-pi/3., -pi/6., pi/2 - pi/4., -pi/4, pi/4. + pi/2, 0.])
+            # start_config = np.array([  -32.78, -167.76, -78.15,  -9.59, 75.21, -137. ])*d2r # right
+            mid_config_origin   = np.array([  -10,  -97.6 , -15.84, -17.65, 75.18, 0. ])*d2r # 66.83 ])
+            mid_base_origin = mid_config_origin[0]/d2r
+            # end_config   = np.array([ -131.26, -150.59, -68.54, -36.02, 74.99, -191.24 ])*d2r
+            duration = 2 # seconds
+            fps = 24
+            # H = ceil(fps*duration)
+            # traj = generate_whip_motion(start_config, end_config, H, 1./fps)
+            t_step = 0.032
+            H = ceil(duration/t_step)
+
+            success = False
+            mid_config = mid_config_origin
+            counter = 0
+            while not success:
+                counter += 1
+                traj, vel, acc, H = generate_whip_motion(start_config, mid_config, end_config, H, 1./fps)
+                # while True:
+                #     H = H - 1
+                #     traj_new = generate_whip_motion(start_config, mid_config, end_config, H, 1./fps)
+                #     if traj_new is None:
+                #         break
+                #     traj = traj_new
+                kf = 51
+                ur5.set_config(start_config)
+                ur5.keyframe_insert(1)
+                bpy.context.scene.frame_set(1)
+
+                # 2a. A keyframe motion of held link to put in gripper (start kf = 1, end kf = 50)
+                held_link.keyframe_insert(data_path="location")
+                held_link.keyframe_insert(data_path="rotation_euler")
+                bpy.context.scene.rigidbody_world.enabled = True
+                bpy.context.scene.rigidbody_world.point_cache.frame_start = 1
+                target_loc = (ur5.gripper.right_inner_finger_pad.matrix_world.translation + ur5.gripper.left_inner_finger_pad.matrix_world.translation)/2
+                at = target_loc - held_link.matrix_world.translation
+                print(at)
+                take_action(held_link, at, keyf, 50-keyf)
+                ur5.keyframe_insert(keyf)
+                # 2b. Set keyframe to ur5
+                ur5.keyframe_insert(51)
+                for i in range(1, 52):
+                    bpy.context.scene.frame_set(i)
+
+                # 3. Add rigid-body constraint to attach held end to ur5.gripper.gripper_base
+                # Inverse the transform matrix to make the transform correct
+                held_link.parent = ur5.gripper.right_inner_finger_pad
+                held_link.matrix_parent_inverse = ur5.gripper.right_inner_finger_pad.matrix_world.inverted()
+
+                # 4. make held link.kinematic=False
+                held_link.rigid_body.kinematic = True
+                # 5. ur5.set_config(...), insert keyframes
+                if traj is not None:
+                    for t in range(traj.shape[0]):
+                        wp = traj[t,:]
+                        # print(wp)
+                        ur5.set_config(wp)
+                        ur5.keyframe_insert(kf)
+                        kf = kf + 1
+                else:
+                    ur5.set_config(start_config)
+                    ur5.keyframe_insert(51)
+                    ur5.set_config(mid_config)
+                    ur5.keyframe_insert(61)
+                    ur5.set_config(end_config)
+                    ur5.keyframe_insert(71)
+
+                for i in range(51, 200):
+                    bpy.context.scene.frame_set(i)
+
+                obstacle_x, obstacle_y, obstacle_z = cylinder.matrix_world.translation
+                success = success_ac(rope, obstacle_x, obstacle_y, obstacle_z, obstacle_radius)
+                if not success:
+                    mid_config = np.array([ mid_base_origin + np.random.uniform(-10, 10),  -97.6 , -15.84, -17.65, 75.18, 0. ])*d2r
+                    for f in range(51, 100):
+                        ur5.keyframe_delete(f)
+
+                print("Success: ", success)
+
+                if counter > 10 and not success:
+                    bpy.context.scene.frame_set(51)
+                    break
+            
+            bpy.context.scene.frame_set(51)
+            mid_pred.append(mid_config)
+            if image:
+                if not os.path.exists("./whip_ur5_sa/images"):
+                    os.makedirs('./whip_ur5_sa/images')
+                # Get the scene
+                scene = bpy.context.scene
+                # Set render resolution
+                scene.render.resolution_x = 256
+                scene.render.resolution_y = 256
+                scene.render.resolution_percentage = 100
+                save_render_path = os.path.join(os.getcwd(), 'whip_ur5_sa/images')
+                bpy.context.scene.render.filepath = os.path.join(save_render_path, 'whip_state_%05d.jpg'%(seq_no))
+                bpy.context.scene.camera.location = (5, 0, 60)
+                bpy.ops.render.render(write_still = True)
+
+            if N > 1:
+                # Delete the obstacle
+                bpy.ops.object.select_all(action='DESELECT')
+                bpy.context.view_layer.objects.active = cylinder
+                cylinder.name="cylinder"
+                bpy.data.objects['cylinder'].select_set(True)
+                bpy.ops.object.delete(use_global=False)
+        if not os.path.exists("./whip_ur5_sa"):
+            os.makedirs('./whip_ur5_sa')
+        np.save(os.path.join(os.getcwd(), 'whip_ur5_sa/a.npy'), np.array(mid_pred))
+    else:
+        device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+        net50 = DistModel(device, 6)
+        state_dict = torch.load('resnet_ur5_model.pth', map_location=device)['model_state_dict']
+        net50.load_state_dict(state_dict)
+        net50.resnet_mean.to(device)
 
         bpy.context.scene.frame_set(1)
         for ac in bpy.data.actions:
@@ -418,8 +568,6 @@ if "__main__" == __name__:
         start_config = np.array([pi/4., 0., pi/6., -pi/4, pi/4., 0.])
         end_config   = np.array([-pi/3., -pi/6., pi/2 - pi/4., -pi/4, pi/4. + pi/2, 0.])
         # start_config = np.array([  -32.78, -167.76, -78.15,  -9.59, 75.21, -137. ])*d2r # right
-        mid_config_origin   = np.array([  -10,  -97.6 , -15.84, -17.65, 75.18, 0. ])*d2r # 66.83 ])
-        mid_base_origin = mid_config_origin[0]/d2r
         # end_config   = np.array([ -131.26, -150.59, -68.54, -36.02, 74.99, -191.24 ])*d2r
         duration = 2 # seconds
         fps = 24
@@ -429,102 +577,83 @@ if "__main__" == __name__:
         H = ceil(duration/t_step)
 
         success = False
-        mid_config = mid_config_origin
-        counter = 0
-        while not success:
-            counter += 1
-            traj, vel, acc, H = generate_whip_motion(start_config, mid_config, end_config, H, 1./fps)
-            # while True:
-            #     H = H - 1
-            #     traj_new = generate_whip_motion(start_config, mid_config, end_config, H, 1./fps)
-            #     if traj_new is None:
-            #         break
-            #     traj = traj_new
-            kf = 51
+        ur5.set_config(start_config)
+        ur5.keyframe_insert(1)
+        bpy.context.scene.frame_set(1)
+
+
+        # 2a. A keyframe motion of held link to put in gripper (start kf = 1, end kf = 50)
+        held_link.keyframe_insert(data_path="location")
+        held_link.keyframe_insert(data_path="rotation_euler")
+        bpy.context.scene.rigidbody_world.enabled = True
+        bpy.context.scene.rigidbody_world.point_cache.frame_start = 1
+        target_loc = (ur5.gripper.right_inner_finger_pad.matrix_world.translation + ur5.gripper.left_inner_finger_pad.matrix_world.translation)/2
+        at = target_loc - held_link.matrix_world.translation
+        print(at)
+        take_action(held_link, at, keyf, 50-keyf)
+        ur5.keyframe_insert(keyf)
+        # 2b. Set keyframe to ur5
+        ur5.keyframe_insert(51)
+        for i in range(1, 52):
+            bpy.context.scene.frame_set(i)
+            if i == 51:
+                if not os.path.exists("./whip_ur5_sa/tests"):
+                    os.makedirs('./whip_ur5_sa/tests')
+                scene = bpy.context.scene
+                scene.render.resolution_x = 256
+                scene.render.resolution_y = 256
+                scene.render.resolution_percentage = 100
+                save_render_path = os.path.join(os.getcwd(), 'whip_ur5_sa/tests')
+                bpy.context.scene.render.filepath = os.path.join(save_render_path, 'whip_test.png')
+                bpy.context.scene.camera.location = (5, 0, 60)
+                bpy.ops.render.render(write_still = True)
+        in_image = Image.open(os.path.join(save_render_path, 'whip_test.png')).convert("RGB")
+        normalize = transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+        preprocess = transforms.Compose([transforms.ToTensor(), normalize])
+        in_image = preprocess(in_image).unsqueeze(0)
+
+        net50.eval()
+        mid_config_pred = net50(in_image.to(device)).sample().detach().numpy()[0]
+
+        traj, vel, acc, H = generate_whip_motion(start_config, mid_config_pred, end_config, H, 1./fps)
+        kf = 51
+        ur5.set_config(start_config)
+        ur5.keyframe_insert(1)
+        bpy.context.scene.frame_set(1)
+
+        # 3. Add rigid-body constraint to attach held end to ur5.gripper.gripper_base
+        # Inverse the transform matrix to make the transform correct
+        held_link.parent = ur5.gripper.right_inner_finger_pad
+        held_link.matrix_parent_inverse = ur5.gripper.right_inner_finger_pad.matrix_world.inverted()
+
+        # 4. make held link.kinematic=False
+        held_link.rigid_body.kinematic = True
+        # 5. ur5.set_config(...), insert keyframes
+        if traj is not None:
+            for t in range(traj.shape[0]):
+                wp = traj[t,:]
+                # print(wp)
+                ur5.set_config(wp)
+                ur5.keyframe_insert(kf)
+                kf = kf + 1
+        else:
             ur5.set_config(start_config)
-            ur5.keyframe_insert(1)
-            bpy.context.scene.frame_set(1)
-
-            # 2a. A keyframe motion of held link to put in gripper (start kf = 1, end kf = 50)
-            held_link.keyframe_insert(data_path="location")
-            held_link.keyframe_insert(data_path="rotation_euler")
-            bpy.context.scene.rigidbody_world.enabled = True
-            bpy.context.scene.rigidbody_world.point_cache.frame_start = 1
-            target_loc = (ur5.gripper.right_inner_finger_pad.matrix_world.translation + ur5.gripper.left_inner_finger_pad.matrix_world.translation)/2
-            at = target_loc - held_link.matrix_world.translation
-            print(at)
-            take_action(held_link, at, keyf, 50-keyf)
-            ur5.keyframe_insert(keyf)
-            # 2b. Set keyframe to ur5
             ur5.keyframe_insert(51)
-            for i in range(1, 52):
-                bpy.context.scene.frame_set(i)
+            ur5.set_config(mid_config)
+            ur5.keyframe_insert(61)
+            ur5.set_config(end_config)
+            ur5.keyframe_insert(71)
 
-            # 3. Add rigid-body constraint to attach held end to ur5.gripper.gripper_base
-            # Inverse the transform matrix to make the transform correct
-            held_link.parent = ur5.gripper.right_inner_finger_pad
-            held_link.matrix_parent_inverse = ur5.gripper.right_inner_finger_pad.matrix_world.inverted()
+        for i in range(51, 200):
+            bpy.context.scene.frame_set(i)
 
-            # 4. make held link.kinematic=False
-            held_link.rigid_body.kinematic = True
-            # 5. ur5.set_config(...), insert keyframes
-            if traj is not None:
-                for t in range(traj.shape[0]):
-                    wp = traj[t,:]
-                    # print(wp)
-                    ur5.set_config(wp)
-                    ur5.keyframe_insert(kf)
-                    kf = kf + 1
-            else:
-                ur5.set_config(start_config)
-                ur5.keyframe_insert(51)
-                ur5.set_config(mid_config)
-                ur5.keyframe_insert(61)
-                ur5.set_config(end_config)
-                ur5.keyframe_insert(71)
+        obstacle_x, obstacle_y, obstacle_z = cylinder.matrix_world.translation
+        success = success_ac(rope, obstacle_x, obstacle_y, obstacle_z, obstacle_radius)
 
-            for i in range(51, 200):
-                bpy.context.scene.frame_set(i)
+        print("Success: ", success)
 
-            obstacle_x, obstacle_y, obstacle_z = cylinder.matrix_world.translation
-            success = success_ac(rope, obstacle_x, obstacle_y, obstacle_z, obstacle_radius)
-            if not success:
-                mid_config = np.array([ mid_base_origin + np.random.uniform(-10, 10),  -97.6 , -15.84, -17.65, 75.18, 0. ])*d2r
-                for f in range(51, 100):
-                    ur5.keyframe_delete(f)
-
-            print("Success: ", success)
-
-            if counter > 10 and not success:
-                bpy.context.scene.frame_set(51)
-                break
-        
         bpy.context.scene.frame_set(51)
-        mid_pred.append(mid_config)
-        if image:
-            if not os.path.exists("./whip_ur5_sa/images"):
-                os.makedirs('./whip_ur5_sa/images')
-            # Get the scene
-            scene = bpy.context.scene
-            # Set render resolution
-            scene.render.resolution_x = 256
-            scene.render.resolution_y = 256
-            scene.render.resolution_percentage = 100
-            save_render_path = os.path.join(os.getcwd(), 'whip_ur5_sa/images')
-            bpy.context.scene.render.filepath = os.path.join(save_render_path, 'whip_state_%05d.jpg'%(seq_no))
-            bpy.context.scene.camera.location = (5, 0, 60)
-            bpy.ops.render.render(write_still = True)
 
-        if N > 1:
-            # Delete the obstacle
-            bpy.ops.object.select_all(action='DESELECT')
-            bpy.context.view_layer.objects.active = cylinder
-            cylinder.name="cylinder"
-            bpy.data.objects['cylinder'].select_set(True)
-            bpy.ops.object.delete(use_global=False)
-    if not os.path.exists("./whip_ur5_sa"):
-        os.makedirs('./whip_ur5_sa')
-    np.save(os.path.join(os.getcwd(), 'whip_ur5_sa/a.npy'), np.array(mid_pred))
 
 
 
